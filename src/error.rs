79e4af00e8bbee06b87850c75e4621763b1eb479
Solomon Jackson <sugarfi@sugarfi.dev>
2020-12-12 12:14:57 -0500

    feat: \basic NO_COLOR support

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,33 @@
+use std::env;
+
 pub fn error(msg: String) {
-    println!("\x1b[0;31m✘ Error: {} ✘\x1b[0m", msg);
+    if env::var("NO_COLOR").unwrap_or(String::new()).len() > 0 {
+        println!("✘ Error: {} ✘", msg);
+    } else {
+        println!("\x1b[0;31m✘ Error: {} ✘\x1b[0m", msg);
+    }
 }
 
 pub fn hint(msg: String) {
-    println!("\x1b[33mHint: {}\x1b[0m", msg);
+    if env::var("NO_COLOR").unwrap_or(String::new()).len() > 0 {
+        println!("Hint: {}", msg);
+    } else {
+        println!("\x1b[33mHint: {}\x1b[0m", msg);
+    }
 }
 
 pub fn warn(msg: String) {
-    println!("\x1b[33mWarn: {}\x1b[0m", msg);
+    if env::var("NO_COLOR").unwrap_or(String::new()).len() > 0 {
+        println!("Warn: {}", msg);
+    } else {
+        println!("\x1b[33mWarn: {}\x1b[0m", msg);
+    }
 }
 
 pub fn success(msg: String) {
-    println!("\x1b[32m✔️ {} ✔️\x1b[0m", msg);
+    if env::var("NO_COLOR").unwrap_or(String::new()).len() > 0 {
+        println!("✔️ {} ✔️", msg);
+    } else {
+        println!("\x1b[32m✔️ {} ✔️\x1b[0m", msg);
+    }
 }
--- src/prompt.rs
+++ src/prompt.rs
@@ -1,20 +1,35 @@
 use crate::eval;
 use crate::session::SessionType;
+use std::env;
 
 pub fn prompt(ctx: &eval::Context) -> String {
-    return format!(
-        "\x1b[35m{} \x1b[34m{}{} \x1b[36msession:[{}]{} \x1b[0m> ",
-        ctx.all_sessions[&ctx.session].get_cwd(),
-        match ctx.status {
-            0 => "\x1b[32m",
-            2 => "\x1b[33m",
-            _ => "\x1b[31m",
-        },
-        ctx.status,
-        ctx.session,
-        match ctx.all_sessions[&ctx.session].get_type() {
-            SessionType::Local => "local",
-            SessionType::Web => "web",
-        }
-    );
+    return
+        if env::var("NO_COLOR").unwrap_or(String::new()).len() > 0 {
+            format!(
+                "{} {} [{}]{} > ",
+                ctx.all_sessions[&ctx.session].get_cwd(),
+                ctx.status,
+                match ctx.all_sessions[&ctx.session].get_type() {
+                    SessionType::Local => "local",
+                    SessionType::Web => "web",
+                },
+                ctx.session
+            )
+        } else {
+            format!(
+                "\x1b[35m{} \x1b[34m{}{} \x1b[36msession:[{}]{} \x1b[0m> ",
+                ctx.all_sessions[&ctx.session].get_cwd(),
+                match ctx.status {
+                    0 => "\x1b[32m",
+                    2 => "\x1b[33m",
+                    _ => "\x1b[31m",
+                },
+                ctx.status,
+                ctx.session,
+                match ctx.all_sessions[&ctx.session].get_type() {
+                    SessionType::Local => "local",
+                    SessionType::Web => "web",
+                }
+            )
+        };
 }
